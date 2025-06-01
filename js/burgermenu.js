7cd6d04f2e11f61e42ee88293eb47afb0db046b0
bonsaitrees <cyruzlundgren@gmail.com>
2025-06-01 16:38:11 +0200

    Delete js directory

--- js/burgermenu.js
+++
@@ -1,9 +0,0 @@
-let subMenu = document.querySelector(".sub-menu");
-
-window.addEventListener("click", (e) => {
-if (e.target.closest(".toggle")) {
-    subMenu.style.display = "flex";
-    } else {
-    subMenu.style.display = "none";
-    }
-});
